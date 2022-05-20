10be5413af7987e64fd3eeca7a1d9c7ee8b1124c
ONEO <oneo.me@outlook.com>
2022-05-20 14:31:33 +0800

    添加控制按钮

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,76 @@
 <script lang="ts">
     import { appWindow } from "@tauri-apps/api/window";
-    import Beach_Regular from "svelte-fluentui-icons/icons/Beach_Regular.svelte";
+    import Dismiss_Regular from "svelte-fluentui-icons/icons/Dismiss_Regular.svelte";
+    import Square_Regular from "svelte-fluentui-icons/icons/Square_Regular.svelte";
+    import SquareMultiple_Regular from "svelte-fluentui-icons/icons/SquareMultiple_Regular.svelte";
+    import LineHorizontal1_Regular from "svelte-fluentui-icons/icons/LineHorizontal1_Regular.svelte";
+    import AppContent from "./AppContent.svelte";
 
     document.body.className =
-        "antialiased bg-white text-slate-600 text-base select-none";
-    document.body.addEventListener("contextmenu", (e) => e.preventDefault());
+        "antialiased bg-white text-slate-600 text-base select-none w-screen h-screen";
+
+    if (import.meta.env.DEV !== true)
+        document.body.addEventListener("contextmenu", (e) =>
+            e.preventDefault()
+        );
 
     let title = "Tauri Template";
     let version = "0.1.0";
 
     appWindow.setTitle(title + " " + version);
+
+    let isMaximized = false;
+
+    function minimize() {
+        appWindow.minimize();
+    }
+    function maximize() {
+        appWindow.toggleMaximize();
+        isMaximized = !isMaximized;
+    }
+    function close() {
+        appWindow.close();
+    }
 </script>
 
-<div class="h-screen flex items-center place-content-center">
-    <div class="flex flex-col items-center space-y-6">
-        <Beach_Regular size={180} class="text-blue-600" />
-        <h1 class="text-4xl font-bold">Hello Tauri</h1>
-        <p>Vite + Svelte + TS + Tauri + Tailwindcss</p>
+<div class="flex flex-col w-screen h-screen">
+    <div class="flex bg-slate-100">
+        <div
+            data-tauri-drag-region
+            class="flex-1 mx-2 my-1.5 text-xs font-bold"
+        >
+            {title}
+        </div>
+        <div class="flex">
+            <div
+                on:click={() => minimize()}
+                class="flex items-center justify-center w-8 bg-slate-200[0]
+                hover:bg-slate-300 active:bg-slate-200"
+            >
+                <LineHorizontal1_Regular class="mt-1" size={14} />
+            </div>
+            <div
+                on:click={() => maximize()}
+                class="flex items-center justify-center w-8
+        bg-slate-200[0] hover:bg-slate-300 active:bg-slate-200"
+            >
+                {#if isMaximized}
+                    <SquareMultiple_Regular size={16} />
+                {:else}
+                    <Square_Regular size={16} />
+                {/if}
+            </div>
+            <div
+                on:click={() => close()}
+                class="flex items-center justify-center w-8
+    bg-slate-200[0] hover:bg-slate-300 active:bg-slate-200"
+            >
+                <Dismiss_Regular size={18} />
+            </div>
+        </div>
+    </div>
+    <div class="flex-1 w-full h-full overflow-hidden">
+        <AppContent />
     </div>
 </div>
 
---
+++ src/AppContent.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Beach_Regular from "svelte-fluentui-icons/icons/Beach_Regular.svelte";
+</script>
+
+<div class="h-full flex justify-center items-center">
+    <div class="flex flex-col items-center gap-3">
+        <Beach_Regular size={180} class="text-blue-600" />
+        <h1 class="text-4xl font-bold">Hello Tauri</h1>
+        <p>Vite + Svelte + TS + Tauri + Tailwindcss</p>
+    </div>
+</div>
